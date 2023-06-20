875d5ff303dca8a10c6092ec69e4b7f488b51c5e
Noah Stasuik <noah.stasuik@gmail.com>
2023-06-19 21:43:44 -0700

    Adding bootstrap and finance updates

--- farm-forward/src/finance.js
+++ farm-forward/src/finance.js
@@ -1,41 +1,31 @@
 import React from 'react';
 
-function Finance() {
+function FinancePage() {
   return (
     <div>
-      <h1>Finance Page</h1>
 
-      <div className="category">
-        <h2>Category 1</h2>
+      <div className="eligible-loans">
+        <h2>Eligible Financial Opportunities</h2>
         <ul>
-          <li>Bullet point 1</li>
-          <li>Bullet point 2</li>
-          <li>Bullet point 3</li>
+          <li>Loan/Grant 1</li>
+          <li>Loan/Grant 2</li>
+          <li>Loan/Grant 3</li>
+          {/* Add more eligible loans/grants */}
         </ul>
       </div>
 
-      <div className="category">
-        <h2>Category 2</h2>
+      <div className="upcoming-grants">
+        <h2>Upcoming Grants/Government Funding</h2>
         <ul>
-          <li>Bullet point 1</li>
-          <li>Bullet point 2</li>
-          <li>Bullet point 3</li>
+          <li>Grant/Funding 1</li>
+          <li>Grant/Funding 2</li>
+          <li>Grant/Funding 3</li>
+          {/* Add more upcoming grants/funding */}
         </ul>
       </div>
-
-      <div className="category">
-        <h2>Category 3</h2>
-        <ul>
-          <li>Bullet point 1</li>
-          <li>Bullet point 2</li>
-          <li>Bullet point 3</li>
-        </ul>
-      </div>
-
-      {/* Add more categories and bullet points */}
       
     </div>
   );
 }
 
-export default Finance;
+export default FinancePage;
