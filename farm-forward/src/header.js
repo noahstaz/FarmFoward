52ef0e595551a1922c4eaf4f6a606dbdbbb31525
Aleen Hasnani <aleen@xlenz.us>
2023-06-20 04:12:32 -0600

    home

--- farm-forward/src/header.js
+++ farm-forward/src/header.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import './header.css'
-import Photo from './images/logo192.png';
+import Photo from './images/FarmForwardLogo.png';
 
 const Header = ({loggedInUser }) => {
   return (
-    <header>
+    <header className='header'>
       <div className="header-left">
         <img src={Photo} alt="Company Logo" />
-        <span className="company-name">Farm Forwarding</span>
+        {/* <span className="company-name">Farm Forward</span> */}
       </div>
       <nav className="header-middle">
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/grants">Grants</a></li>
-          <li><a href="/resources">Resources</a></li>
-          <li><a href="/equipment">Equipment</a></li>
+          <li className='mx-3'><a href="/grants">Grants</a></li>
+          <li className='mx-3'><a href="/resources">Resources</a></li>
+          <li className='mx-3'><a href="/equipment">Equipment</a></li>
         </ul>
       </nav>
       <div className="header-right">
-        <span className="username">Hello, User</span>
+        <span className="username">Hello John</span>
       </div>
     </header>
   );
