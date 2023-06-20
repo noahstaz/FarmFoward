e744838ffa551d33fdd88e3d14c310f1ac7ec42e
Raphael <graphael.dr@gmail.com>
2023-06-19 23:59:02 -0600

    Fixed onboarding form

--- farm-forward/src/onboardingScreen.js
+++ farm-forward/src/onboardingScreen.js
@@ -1,64 +1,85 @@
 import React, { useState } from 'react';
 import './onboardingScreen.css';
 
-const SignUpForm = () => {
-  const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [email, setEmail] = useState('');
+const OnboardingScreen = () => {
+  const [formIndex, setFormIndex] = useState(0);
+  const [formData, setFormData] = useState({
+    name: '',
+    phone: '',
+    email: '',
+    password: ''
+  });
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handlePhoneNumberChange = (e) => {
-    setPhoneNumber(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const handleNext = () => {
+    setFormIndex(formIndex + 1);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Perform form submission logic here
+    // Handle form submission logic
+  };
+
+  const renderForm = () => {
+    switch (formIndex) {
+      case 0:
+        return (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              placeholder="Name"
+            />
+            <input
+              type="text"
+              name="phone"
+              value={formData.phone}
+              onChange={handleInputChange}
+              placeholder="Phone"
+            />
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              placeholder="Password"
+            />
+            <button type="submit">Next</button>
+          </form>
+        );
+      case 1:
+        // Render the second form component here
+        return (
+          <form onSubmit={handleSubmit}>
+            {/* Second form fields */}
+            <button type="submit">Next</button>
+          </form>
+        );
+      // Add additional cases for more forms as needed
+      default:
+        return null;
+    }
   };
 
   return (
     <div className="centered-box">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={handleNameChange}
-            placeholder="Name"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="phoneNumber"
-            value={phoneNumber}
-            onChange={handlePhoneNumberChange}
-            placeholder="Phone Number"
-          />
-        </div>
-        <div>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            placeholder="Email"
-          />
-        </div>
-        <div className="button-container">
-          <button type="submit" className="next-button">Next</button>
-        </div>
-      </form>
+      <div className={`slide-container ${formIndex > 0 ? 'slide-left' : ''}`}>
+        {renderForm()}
+      </div>
     </div>
   );
 };
 
-export default SignUpForm;
+export default OnboardingScreen;
